b29cb7c31fceb7fe910628910dbab9494872be2b
Gumelo023 <gustavomelopereira02@gmail.com>
2024-09-08 17:32:48 -0300

    SOS MECANICO

---
+++ app/src/main/java/com/tcc/tccmecanico/Tela_Inicial.java
@@ -0,0 +1,14 @@
+package com.tcc.tccmecanico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tela_Inicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_inicial);
+    }
+}
