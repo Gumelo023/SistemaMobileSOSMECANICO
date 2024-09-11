3374da0f0aba060b349f603e95bf17a13a32c89c
Gumelo023 <gustavomelopereira02@gmail.com>
2024-09-10 23:05:47 -0300

    SOS MECANICO

---
+++ app/src/main/java/com/tcc/tccmecanico/CatalogoMecanicos.java
@@ -0,0 +1,14 @@
+package com.tcc.tccmecanico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CatalogoMecanicos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalogo_mecanicos);
+    }
+}
