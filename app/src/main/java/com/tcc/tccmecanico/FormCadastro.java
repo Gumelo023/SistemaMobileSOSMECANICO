74210c72bd1425fec21066c442df4be32ca25f26
Gumelo023 <gustavomelopereira02@gmail.com>
2024-09-20 12:18:28 -0300

    TCC_SOS_MECANICO

--- app/src/main/java/com/tcc/tccmecanico/FormCadastro.java
+++ app/src/main/java/com/tcc/tccmecanico/FormCadastro.java
@@ -2,7 +2,10 @@ package com.tcc.tccmecanico;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FormCadastro extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class FormCadastro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_cadastro);
+        Button button = findViewById(R.id.button_cad);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(FormCadastro.this, Tela_Inicial.class);
+                startActivity(it);
+            }
+        });
     }
 }
