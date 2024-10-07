07d0794c26f32041f53975c014652d44c7013ff2
Gumelo023 <gustavomelopereira02@gmail.com>
2024-10-07 09:00:43 -0300

    update

---
+++ app/src/main/java/com/tcc/tccmecanico/Tela_Inicial.java
@@ -0,0 +1,55 @@
+package com.tcc.tccmecanico;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Tela_Inicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_inicial);
+
+        CardView card_continuar = findViewById(R.id.card_continuar);
+        card_continuar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Tela_Inicial.this, MenuLateral.class);
+                startActivity(intent);
+
+            }
+
+
+        });
+
+        CardView card_fazer_cad = findViewById(R.id.card_fazer_cad);
+        card_fazer_cad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Tela_Inicial.this, FormCadastro.class);
+                startActivity(intent);
+
+            }
+
+
+        });
+
+        CardView card_login = findViewById(R.id.card_login);
+        card_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Tela_Inicial.this, FormLogin.class);
+                startActivity(intent);
+
+            }
+
+
+        });
+
+
+    }
+}
