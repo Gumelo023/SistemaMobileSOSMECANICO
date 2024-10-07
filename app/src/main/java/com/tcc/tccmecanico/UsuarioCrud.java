359b55503f92d12d5de0570c5cb5f8a3e7c420d2
robinho00 <rs.robsonsilva.28@gmail.com>
2024-10-07 10:36:20 -0300

    Ajustes

--- app/src/main/java/com/tcc/tccmecanico/UsuarioCrud.java
+++ app/src/main/java/com/tcc/tccmecanico/UsuarioCrud.java
@@ -3,16 +3,30 @@ package com.tcc.tccmecanico;
 import android.content.Context;
 
 import java.sql.PreparedStatement;
+import java.sql.SQLData;
+
+public class UsuarioCrud {
+
+
+     public static int InserirUsuario (Usuario usuario, Context ctx){
+
+          int resposta = 0;
+
+          try{
+               PreparedStatement pst = Conexao.conectar(ctx).prepareStatement(
+                     SQLData:"Insert Into Mecanico( nome, telefone , cpf , atendimento , usuario_id , statusMecanico)" + "values (?,?,?,?,?,?)");
+
+             pst.setString(parameterIndex: 1, Usuario.getNome());
+              pst.setString(parameterIndex: 1, Usuario.getEmail());
 
 
 
 
 
+          }
 
 
 
 
-
-
-
-
+     }
+}
