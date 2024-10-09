4c8e8633eb235f34605d009f1b94b00756d4c501
robinho00 <rs.robsonsilva.28@gmail.com>
2024-10-09 10:07:07 -0300

    crud

--- app/src/androidTest/java/Cadastro.java
+++ app/src/androidTest/java/Cadastro.java
@@ -4,6 +4,8 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class Cadastro extends AppCompatActivity {
 
     EditText editNome, editEmail, editSenha;
@@ -19,6 +21,30 @@ public class Cadastro extends AppCompatActivity {
         senha id =editSenha
 
                 Botão cadastro = btn_cadastro
+
+                        if (res <=0){
+                            Snackbar.make(btn_cadastro, text:"Dados não inseridos!" ,  Snackbar.LENGTH_LONG)
+
+                        }else {
+                            Snackbar.make()
+
+                  set
+
+
+
+                        editNome =(EditText) findViewById(R.id.editNome);
+
+
+
+
+
+
+
+
+
+
+
+                        }
 
 
 
