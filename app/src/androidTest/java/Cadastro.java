c162ab63be07b5bcb4faf626d5be177c62a4a95e
robinho00 <rs.robsonsilva.28@gmail.com>
2024-10-09 11:01:10 -0300

    crud

--- app/src/androidTest/java/Cadastro.java
+++ app/src/androidTest/java/Cadastro.java
@@ -1,10 +1,16 @@
+import static android.app.ProgressDialog.show;
+
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.tcc.tccmecanico.R;
+import com.tcc.tccmecanico.Usuario;
+import com.tcc.tccmecanico.UsuarioCrud;
 
 public class Cadastro extends AppCompatActivity {
 
@@ -14,7 +20,8 @@ public class Cadastro extends AppCompatActivity {
 
 
     @Override
-
+    Object btn_cadastrar;
+    Object user = new;
     protected void onCreate(Bundle saveInstanceState){
 
         email id =editEmail
@@ -23,16 +30,43 @@ public class Cadastro extends AppCompatActivity {
                 Botão cadastro = btn_cadastro
 
                         if (res <=0){
-                            Snackbar.make(btn_cadastro, text:"Dados não inseridos!" ,  Snackbar.LENGTH_LONG)
+                            int text;
+                            Snackbar.make(btn_cadastrar, text:"Dados não inseridos!" ,  Snackbar.LENGTH_LONG)
 
                         }else {
                             Snackbar.make()
 
-                  set
+
+                            editEmail = (EditText) findViewById(R.id.editEmail);
+                            editSenha = (EditText) findViewById(R.id.editSenha);
+                            btn_cadastro = (Button) findViewById(R.id.btn_cadastrar);
+
+
+                            @Override
+                                    public void onClick (View)
+
+                                    Usuario user = new Usuario(
+                                 editEmail.getText().toString(),
+                                    editSenha.getText().toString(),
+
+                            );
+
+                            int res = UsuarioCrud.InserirUsuario(user, getBaseContext());
+                            if (res<=0) {
+
+                                Snackbar.make(btn_cadastrar, text:
+                                "Dados não inseridos!", Snackbar.LENGTH_LONG).show();
+                            }else {
+                                Snackbar.make(btn_cadastrar, text:
+                                "Dados Cadastrados com Sucesso", Snackbar.LENGTH_LONG).show();
+                            }
 
 
 
-                        editNome =(EditText) findViewById(R.id.editNome);
+
+
+
+                            }
 
 
 
